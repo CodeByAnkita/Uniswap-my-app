8f8ca2586bb5071dc76f1452b1a48060c5291c14
ankitaeth <ankitacode11@gmail.com>
2023-09-19 15:11:54 +0530

    Initial file Structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//INTERNAL IMPORT
+import { NavBar } from "../components/index";
+
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <NavBar />
+    <Component {...pageProps} />
+  </div>
+);
+export default MyApp;
